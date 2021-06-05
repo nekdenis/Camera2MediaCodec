a24f6f4043820db3a6432a25b2f6406748762c73
dnek <dnek@lyft.com>
2021-06-04 17:01:18 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Camera2MediaCodec
-Android Video Recording With MediaCodec and GLSurfaceView
+Android Video Recording with Camera2, MediaCodec and GLSurfaceView
+
+There is nothing new in the code. I've mixed existing [Google's Camear2Video sample](https://github.com/android/camera-samples/blob/master/Camera2Video) and [Google's CameraCaptureActivity from experimental repo Grafika](https://github.com/google/grafika/blob/master/app/src/main/java/com/android/grafika/CameraCaptureActivity.java) to support Camera2 API
+
+Sample might help anyone who is willing to apply their own video effects using OpenGL and then preview it to user and record video into the file. 
---
+++ app/src/main/java/com/example/android/camera2/video/grafika/CameraHandler.java
@@ -0,0 +1,62 @@
+package com.example.android.camera2.video.grafika;
+
+import android.graphics.SurfaceTexture;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import com.example.android.camera2.video.fragments.CameraFragment;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Handles camera operation requests from other threads.  Necessary because the Camera
+ * must only be accessed from one thread.
+ * <p>
+ * The object is created on the UI thread, and all handlers run there.  Messages are
+ * sent from other threads, using sendMessage().
+ */
+public class CameraHandler extends Handler {
+
+
+    public static final int MSG_SET_SURFACE_TEXTURE = 0;
+    public static final String TAG = "CameraHandlerGrafika";
+
+    // Weak reference to the Activity; only access this from the UI thread.
+    private WeakReference<CameraFragment> mWeakActivity;
+
+    public CameraHandler(CameraFragment activity, HandlerThread thread) {
+        super(thread.getLooper());
+        mWeakActivity = new WeakReference<CameraFragment>(activity);
+    }
+
+    /**
+     * Drop the reference to the activity.  Useful as a paranoid measure to ensure that
+     * attempts to access a stale Activity through a handler are caught.
+     */
+    public void invalidateHandler() {
+        mWeakActivity.clear();
+    }
+
+    @Override  // runs on UI thread
+    public void handleMessage(Message inputMessage) {
+        int what = inputMessage.what;
+        Log.d(TAG, "CameraHandler [" + this + "]: what=" + what);
+
+        CameraFragment activity = mWeakActivity.get();
+        if (activity == null) {
+            Log.w(TAG, "CameraHandler.handleMessage: activity is null");
+            return;
+        }
+
+        switch (what) {
+            case MSG_SET_SURFACE_TEXTURE:
+                activity.handleSetSurfaceTexture((SurfaceTexture) inputMessage.obj);
+                break;
+            default:
+                throw new RuntimeException("unknown msg " + what);
+        }
+    }
+}
